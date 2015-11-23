6e0f21d85804716770b4fe6a597deaf20554ac35
predecessor <renqianbei@sina.cn>
2015-11-23 10:59:51 +0800

    //新工程

---
+++ SingleProgrammersS/SingleProgrammersS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SingleProgrammersS
+//
+//  Created by 任前辈 on 15/11/23.
+//  Copyright (c) 2015年 SingleProgrammers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
